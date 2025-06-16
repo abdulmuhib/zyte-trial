d33cd5b96442fc29cac26538117ed6b8194d6ad7
Abdul Muhib <abdul.muhib@ili.digital>
2025-06-16 15:31:44 +0500

    feat: added-files

---
+++ trial/items/bookspider_item.py
@@ -0,0 +1,20 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+from ..constants.bookspider_constants import BookSpiderConstants
+
+
+class BookSpiderItem(scrapy.Item):
+    fields = {
+        BookSpiderConstants.FIELD_URL: scrapy.Field(),
+        BookSpiderConstants.FIELD_TITLE: scrapy.Field(),
+        BookSpiderConstants.FIELD_PRICE: scrapy.Field(),
+        BookSpiderConstants.FIELD_PRICE_EXCL: scrapy.Field(),
+        BookSpiderConstants.FIELD_PRICE_INCL: scrapy.Field(),
+        BookSpiderConstants.FIELD_AVAIL: scrapy.Field(),
+        BookSpiderConstants.FIELD_STARS: scrapy.Field(),
+    }
